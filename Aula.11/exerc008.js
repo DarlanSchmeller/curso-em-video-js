87c767f3711a6d2a5021913852f9912a8a648267
MM Tecnologia <mmtecnologia@MMLAPTOP00015.local>
2024-01-30 17:39:36 -0300

    Working with if statements and functions

---
+++ Aula.11/exerc008.js
@@ -0,0 +1,10 @@
+console.log('O console funcionou corretamente.')
+var vel = 65
+
+console.log(`A velocidado do seu carro é ${vel}Km/h.`)
+
+if (vel >= 60) { // condição 
+    console.log('Você ultrapassou o limite de velocidade, você foi multado.') // Se a condição for verdade
+} else { 
+    console.log('Você está dentro do limite de velocidade.') // Se a condição for falsa.
+}
